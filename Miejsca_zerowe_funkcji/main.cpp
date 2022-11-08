5776d6d97bf90829702dd0fc453c1dbf700dfe49
dezinter8 <michal.chruslicki01@gmail.com>
2022-11-09 00:05:50 +0100

    2.0

--- Miejsca_zerowe_funkcji/main.cpp
+++ Miejsca_zerowe_funkcji/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    float a , b , c , x , y , z;
+    float a , b , c , x , y , z, x1, x2, delta;
     cout << "Program do obliczania miejsca zerowych funkcji liniowej." << endl;
     cout << "Podaj a:";
     cin >> a;
@@ -29,6 +30,27 @@ int main()
     cout << "Postac kierunkowa funkcji liniowej: " << endl;
     y=(a*x)+b;
     cout << "y = " << y;
+
+    cout << endl;
+    cout << " Program obliczajacy miejsca zerowe funkcji kwadratowej" << endl;
+
+    if (a!=0) {
+        delta= (b*b)-4*a*c;
+        x1=(-b-sqrt(delta))/2*a;
+        x2=(-b+sqrt(delta))/2*a;
+
+        cout << "Delta " << delta << endl;
+
+        if (delta<0) {
+            cout << " Brak miejsc zerowych" << endl;
+        } else if (delta==0) {
+            cout << "Jedno miejsce zerowe: " << -b/2*a << endl;
+        } else {
+            cout << "Dwa miejsca zerowe: x1 = " << x1 << " x2 = " << x2 << endl;
+        }
+    } else {
+        cout << "A jest rowne 0" << endl;
+    }
 
     return 0;
 }
